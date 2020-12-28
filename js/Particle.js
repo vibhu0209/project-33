78ff8d954840464953c97685534548de8e7a750d
VAIBHAV JAIN <64785307+vibhu0209@users.noreply.github.com>
2020-12-28 17:50:52 +0530

    Add files via upload

---
+++ js/Particle.js
@@ -0,0 +1,25 @@
+class Particle{
+    constructor(x , y ){
+        var options = {
+            restitution : 0.4
+        }
+        this.r = 10
+        this.body = Bodies.circle(x , y , this.r , options);
+        this.colour = color(random(0 , 255) , random(0 , 255) , random(0 , 255));
+        World.add(world , this.body);
+
+    }
+    display(){
+     var pos = this.body.position;
+     var angle = this.body.angle;
+
+     push();
+     translate(pos.x , pos.y);
+     rotate(angle);
+     noStroke();
+     fill(this.colour);
+     ellipseMode(RADIUS);
+     ellipse(0 ,0 ,this.r, this.r);   
+     pop(); 
+    }
+}
